7456287790ec04faab12cb7ef4f8458a7799d88a
Domantas <dnasknow@gmail.com>
2024-10-21 15:58:38 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String text;
-        int o = 0;
+        char ch;
+        int n = 0;
         Scanner sc = new Scanner(System.in);
         text = sc.nextLine();
+        ch = sc.nextLine().charAt(0);
+        text = text.toLowerCase();
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == 'o') {
-                o++;
+            if (text.charAt(i) == ch) {
+                n++;
             }
         }
-        System.out.println("The character 'o' appears " + o + " times in the string.");
+        System.out.println("The character " + ch + " appears " + n + " times in the string.");
     }
 }
